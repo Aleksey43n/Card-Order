f5513bcc985ca7c87eb411c1430c04e1b9226937
Alex Norin <aleksey43n@gmail.com>
2022-05-08 23:28:31 +0300

    inital commit

---
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -0,0 +1,45 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CardOrderTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp2() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void close() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void test() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type=\"text")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[type=\"tel")).sendKeys("+79999999999");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+}
